c4b82ad0dfeeb3248f579a9ae5a475a515f9790f
Claudiomildo Ventura <claudiomildo@hotmail.com>
2026-04-17 18:39:35 -0300

    feat: Simplify interface declarations by consolidating GUIDs on the same line

--- port/input/ArchetypeServicePort.pas
+++ port/input/ArchetypeServicePort.pas
@@ -5,8 +5,7 @@ interface
 uses Archetype;
 
 type
-  IArchetypeService = interface
-    ['{A1B2C3D4-E5F6-47A8-9B0C-1234567890AB}']
+  IArchetypeService = interface ['{A1B2C3D4-E5F6-47A8-9B0C-1234567890AB}']
     function Execute: TArchetype;
   end;
 
--- port/output/ArchetypeControllerPort.pas
+++ port/output/ArchetypeControllerPort.pas
@@ -3,8 +3,7 @@ unit ArchetypeControllerPort;
 interface
 
 type
-  IArchetypeController = interface
-    ['{A1B2C3D4-E5F6-47A8-9B0C-1D2E3F4A5B6C}']
+  IArchetypeController = interface ['{A1B2C3D4-E5F6-47A8-9B0C-1D2E3F4A5B6C}']
     function GenerateSolution: string;
   end;
 
